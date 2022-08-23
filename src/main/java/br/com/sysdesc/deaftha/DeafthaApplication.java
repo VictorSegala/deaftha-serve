5bf7a63af6680e88f32f625767b098b691ff7ac5
Leandro Jose Zanatta <leandro.zanatta@softplan.com.br>
2022-08-22 22:16:10 -0300

    doc: commit inicial

---
+++ src/main/java/br/com/sysdesc/deaftha/DeafthaApplication.java
@@ -0,0 +1,13 @@
+package br.com.sysdesc.deaftha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeafthaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeafthaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sysdesc/deaftha/DeafthaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sysdesc.deaftha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeafthaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
